875e6c8e6f5da3eaf0d9e10b414c27fc5fa67151
Tolleiv Nietsch <tolleiv.nietsch@aoemedia.de>
2013-07-04 09:57:28 +0200

    Initial commit - tests are green ;)

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Green test',
+	'description' => 'Extension which is used to carrie an green unit test to test the ci "workflow" itself.',
+	'category' => 'misc',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => 0,
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Tolleiv Nietsch',
+	'author_email' => 'tolleiv.nietsch@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '6.0.0-6.1.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
